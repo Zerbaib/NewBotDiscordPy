bff4a067d480a2355cb33f47de8446247326ca78
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2022-11-07 12:00:41 +0100

    test 1

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 import json
+import os
+import sys
 
-with open("config.json") as file:
-    config = json.load(file)
+if not os.path.isfile("config.json"):
+    sys.exit("ERROR: Le fichier 'config.json' n'a pas ete trouve !")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
 
 intents = discord.Intents.default()
-bot = Bot(command_prefix=config["bot_prefix"], intents=intents)
+bot = Bot(command_prefix=config["prefix"], intents=intents)
 bot.remove_command("help")
 
 async def on_ready(self):
-    print(f'Logged on as {self.user}!')
+    print(f"Logged on as {self.user}!")
+
+if __name__ == "__main__":
+    for file in os.listdir("./cmd"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                bot.load_extension(f"cmd.{extension}")
+                print(f"Extension chargee '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Echec du chargement de l'extension {extension}\n{exception}")
 
 async def on_message(message):
     print(f'Message from {message.author}: {message.content}')
